1a779fc9e39fa4281c40c6dd5bd906c9662338b0
KshishAshish <17uec066@lnmiit.ac.in>
2020-11-08 06:46:09 +0530

    my-app

--- src/pages/System/System.js
+++ src/pages/System/System.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import './System.css'
 
 export class System extends Component {
     render() {
         return (
-            <div className="system-page">
-                khskdfh
+            <div className="">
+                <div className="settings-page text-center">
+                    kdhfkfdg
+                </div>
             </div>
         )
     }
